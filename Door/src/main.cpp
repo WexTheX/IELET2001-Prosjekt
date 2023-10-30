0a7ba74448c77699badcbe883582c4bbcb5c3853
WexTheX <wexthex@gmail.com>
2023-10-30 20:31:38 +0100

    Door: Deleted missed variable

--- Door/src/main.cpp
+++ Door/src/main.cpp
@@ -13,9 +13,6 @@ const char *VARIABLE_LABEL  = "Motion";   // Put here your Variable label to whi
 
 const int PUBLISH_FREQUENCY = 5000; // Update rate in milliseconds
 
-unsigned long timer;
-uint8_t analogPin = 34; // Pin used to read data from GPIO34 ADC_CH6.
-
 Ubidots ubidots(UBIDOTS_TOKEN);
 
 void callback(char *topic, byte *payload, unsigned int length)
