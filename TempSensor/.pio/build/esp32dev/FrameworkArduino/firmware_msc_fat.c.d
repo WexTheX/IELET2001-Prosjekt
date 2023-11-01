7f43b909288a9de01bc97e729064e48e3e34ee47
Odin <wexthex@gmail.com>
2023-11-01 21:12:04 +0100

    Delete TempSensor/.pio directory

--- TempSensor/.pio/build/esp32dev/FrameworkArduino/firmware_msc_fat.c.d
+++
@@ -1,3 +0,0 @@
-.pio/build/esp32dev/FrameworkArduino/firmware_msc_fat.c.o: \
- C:/Users/asbjo/.platformio/packages/framework-arduinoespressif32/cores/esp32/firmware_msc_fat.c \
- C:/Users/asbjo/.platformio/packages/framework-arduinoespressif32/cores/esp32/firmware_msc_fat.h
--- TempSensor/.pio/build/esp32dev/FrameworkArduino/libb64/cencode.c.d
+++
@@ -1,3 +0,0 @@
-.pio/build/esp32dev/FrameworkArduino/libb64/cencode.c.o: \
- C:/Users/asbjo/.platformio/packages/framework-arduinoespressif32/cores/esp32/libb64/cencode.c \
- C:/Users/asbjo/.platformio/packages/framework-arduinoespressif32/cores/esp32/libb64/cencode.h
